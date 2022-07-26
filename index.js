26b3f8d9649c176a46b074dfb549a671bbe7baaf
iMarkZz <98038313+iMarkZz@users.noreply.github.com>
2022-07-26 13:22:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 // Replace the arguments below according to your preference.
 
 // space, scary, military, romantic, cowboy, fantasy, superhero
-favouriteMovieGenre("romantic")
+favouriteMovieGenre("fantasy")
 
 // watermelon, tomato, banana, orange, avocado, blueberry
 favouriteFruit("watermelon")
